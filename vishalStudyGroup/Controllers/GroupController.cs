92dea81e2737aaefdea73e82b3c62fa6727fdff8
Vishal Bhagwan <u23684722@tuks.co.za>
2025-03-01 22:51:30 +0200

    Fixed display issues

--- vishalStudyGroup/Controllers/GroupController.cs
+++ vishalStudyGroup/Controllers/GroupController.cs
@@ -15,6 +15,10 @@ namespace vishalStudyGroup.Controllers
             List<Models.GroupModel> group = new List<Models.GroupModel>();
 
             group.Add(new Models.GroupModel { StudentNum = "u23684722", FirstName = "Vishal", LastName = "Bhagwan", Email = "u23684722@tuks.co.za"});
+            group.Add(new Models.GroupModel { StudentNum = "u23585600", FirstName = "Callum", LastName = "Molver", Email = "u23585600@tuks.co.za" });
+            group.Add(new Models.GroupModel { StudentNum = "u24706672", FirstName = "Nikhil", LastName = "Sunker", Email = "u24706672@tuks.co.za" });
+            group.Add(new Models.GroupModel { StudentNum = "u23601711", FirstName = "Tyla", LastName = "Naidoo", Email = "u23601711@tuks.co.za" });
+            group.Add(new Models.GroupModel { StudentNum = "u23682044", FirstName = "Musawenkosi", LastName = "Mathebula", Email = "u23682044@tuks.co.za" });
 
             return View(group);
         }
