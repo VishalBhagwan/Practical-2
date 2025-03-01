5578bab4b4aa0a099a13a10be1580f5f0333f7b8
Vishal Bhagwan <u23684722@tuks.co.za>
2025-03-01 21:33:04 +0200

    Test

---
+++ vishalStudyGroup/Models/GroupModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace vishalStudyGroup.Models
+{
+    public class GroupModel
+    {
+        [Display(Name = "Student Number")]
+        public int StudnetNum { get; set; }
+
+        [Display(Name = "Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Surname")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+    }
+}
